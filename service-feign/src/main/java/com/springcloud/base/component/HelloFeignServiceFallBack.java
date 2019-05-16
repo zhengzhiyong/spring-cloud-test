88be5d81a1118155c8b83f394b135a08a02f9de9
ZhiYong <zzy_ww@sina.cn>
2019-05-16 23:48:41 +0800

    hystrix 断路器，fallback和fallbackFactory用法

--- service-feign/src/main/java/com/springcloud/base/component/HelloFeignServiceFallBack.java
+++ service-feign/src/main/java/com/springcloud/base/component/HelloFeignServiceFallBack.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Component
-@RequestMapping("/fallback/provider/hello")
+@RequestMapping("/fallback/provider")
 public class HelloFeignServiceFallBack implements HelloFeignService {
+    @RequestMapping("/hello")
     @Override
     public String sayHello(String name) {
         return "say hello to " + name + " failed 熔断";
