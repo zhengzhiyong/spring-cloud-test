baca702da0e3f98f4ad67aece811fd6efb0224c5
zhengzhiyong <zhengzhiyong@jumore.com>
2019-05-16 12:00:02 +0800

    dmeo

--- eureka-consumer-002/src/main/java/com/springcloud/eureka/consumer/controller/UserController.java
+++ eureka-consumer-002/src/main/java/com/springcloud/eureka/consumer/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.springcloud.eureka.consumer.controller;
 
 import com.springcloud.base.po.User;
+import com.springcloud.base.service.UserFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
